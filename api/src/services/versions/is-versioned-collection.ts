1be79906f2847b81ef51c4e84ccc3d16a6da9619
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2026-04-01 12:34:35 -0400

    fix isVersionedCollection types

--- api/src/services/versions/is-versioned-collection.ts
+++ api/src/services/versions/is-versioned-collection.ts
@@ -6,5 +6,5 @@ import { toVersionName } from './to-version-name.js';
  * Check whether a version table exists for the given collection.
  */
 export function isVersionedCollection(schema: SchemaOverview, collection: string): boolean {
-	return toBoolean(schema.collections[collection]?.versioning) || schema.collections[toVersionName(collection)];
+	return toBoolean(schema.collections[collection]?.versioning) || !!schema.collections[toVersionName(collection)];
 }
