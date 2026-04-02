d532f2121eedd7512d76b4e05c61067f1bbb8ccf
HZooly <torzuoli.hugo@gmail.com>
2026-04-02 12:32:34 +0200

    feat: add AppTooltip singleton component using Reka-UI

---
+++ app/src/components/app-tooltip.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { TooltipContent, TooltipPortal, TooltipRoot, TooltipTrigger } from 'reka-ui';
+import { useGlobalTooltip } from '@/composables/use-global-tooltip';
+
+const { state, closeTooltip } = useGlobalTooltip();
+</script>
+
+<template>
+	<TooltipRoot :open="state.open" :delay-duration="state.delayDuration" @update:open="(v) => !v && closeTooltip()">
+		<TooltipTrigger as="span" :reference="state.virtualRef" aria-hidden="true" :tabindex="-1" style="display: none" />
+		<TooltipPortal>
+			<TooltipContent
+				id="app-tooltip-content"
+				:side="state.side"
+				:align="state.align"
+				:side-offset="8"
+				class="tooltip"
+				:class="{ inverted: state.inverted, monospace: state.monospace }"
+			>
+				{{ state.content }}
+			</TooltipContent>
+		</TooltipPortal>
+	</TooltipRoot>
+</template>
